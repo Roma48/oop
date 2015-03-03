fa84e797843d2097c301d4715219c7286945e156
Roma Paliy <romapaliy@Romas-Mac-mini.local>
2015-03-03 12:12:25 +0200

    korobka

--- main.php
+++ main.php
@@ -14,7 +14,7 @@ require_once('kulia.php');
 
 $geom = array();
 
-for($i = 0; $i < 10; $i++){
+for($i = 0; $i < 15; $i++){
     $geom[$i] = rand(1,3);
     if ($geom[$i] == 1){
         $figura = new parallelepiped(rand(1,10), rand(1,10), rand(1,10), rand(1,10));
@@ -43,11 +43,27 @@ echo "\n";
                 }
             }
         }
-        return $arr;
+        $sort_geom = array(array($arr[6],$arr[3],$arr[1],$arr[0]), array($arr[10],$arr[7],$arr[4],$arr[2]), array($arr[13],$arr[11],$arr[8],$arr[5]), array($arr[15],$arr[14],$arr[12],$arr[9]));
+
+        return $sort_geom;
     }
 
-echo "sorted array";
-print_r(buble($geom));
+echo "sorted array \n ";
+
+$arr = buble($geom);
+
+
+echo $arr[0][0] . " " .$arr[0][1] . " " .$arr[0][2] . " " .$arr[0][3] . "\n";
+echo $arr[1][0] . " " .$arr[1][1] . " " .$arr[1][2] . " " .$arr[1][3] . "\n";
+echo $arr[2][0] . " " .$arr[2][1] . " " .$arr[2][2] . " " .$arr[2][3] . "\n";
+echo $arr[3][0] . " " .$arr[3][1] . " " .$arr[3][2] . " " .$arr[3][3] . "\n";
+
+
+
+
+
+
+
 
 
 
